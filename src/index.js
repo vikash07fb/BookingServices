76e70a23e05a68b824134faa7a2a01d40638c887
Vikash kumar Rai <coolvikash9572@gmail.com>
2023-08-11 01:07:48 +0530

    First commit with basic setup

---
+++ src/index.js
@@ -0,0 +1,26 @@
+ const express = require('express');
+ const bodyParser = require('body-parser');
+const app = express();
+const {PORT}= require('./config/serverConfig');
+
+const apiRoutes = require('./routes/index');
+
+
+
+const setupAndStartServer =async function (){
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use('/api',apiRoutes);
+     app.listen(PORT,async function(){
+        console.log(`Server Started at ${PORT}`);
+
+        if(process.env.DB_SYNC)
+        {
+            db.sequelize.sync({alter: true});
+        }
+     })
+}
+
+
+setupAndStartServer();
